d3155890659f1311a293e1032d01ee996f8af4ca
john willian aliore <john.jwa123@gmail.com>
2022-04-13 00:22:42 -0300

    aula de java script

---
+++ modulo1/javaScript/index.js
@@ -0,0 +1,39 @@
+//let nome
+//let idade
+
+//console.log(nome)
+//console.log(idade)
+/*O resultado foi "undefined" por não ter atribuido valor nenhum.*/
+
+//let nome = prompt("Qual é o seu nome?");
+//let idade = prompt("Qual é a sua idade?");
+
+//console.log(nome),typeof(nome);
+//console.log(idade),typeof(idade)
+/*Me apareceu o tipo string,que vale tanto para numero quanto para escrita
+porém eu posso converter essa atribuição se a caso eu precise que me apareça só um ou outro.*/
+
+//let nome = prompt("Olá qual é o seu nome")
+//let idade = prompt("Qual sua idade?")
+//alert(`Olá ${nome} Você tem ${idade} Anos`)
+//console.log(nome,idade)
+
+
+//let azul = prompt("Você está usando azul hoje?")
+//let casa = prompt("Você tem casa?")
+//let carro = prompt("Você tem carro?")
+//alert(`Você está usando azul hoje? Resposta: ${azul}`)
+//alert(`Você tem casa? Resposta: ${casa}`)
+//alert(`Você tem casa? Resposta: ${carro}`)
+
+/*O exercicio 2 eu entendi assim espero que esteja certo kkk*/
+
+//let a = 10
+//let b = 25
+//b=a
+//a=b+b+b-5
+
+//console.log("O novo valor de a é", a);
+//console.log("O novo valor de b é",b);
+
+/*bom acho que finalizei tudo.*/
