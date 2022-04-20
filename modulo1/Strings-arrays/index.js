39dd9a51cdd8bd69ec9fa32955abfeda5d9ca781
john willian aliore <john.jwa123@gmail.com>
2022-04-20 00:04:27 -0300

    Arrays

---
+++ modulo1/Strings-arrays/index.js
@@ -0,0 +1,125 @@
+//(1) Indique todas as mensagens impressas no console, SEM EXECUTAR o programa.
+
+//let array
+//console.log('a. ', array)
+//resp(aqui apenas está criada uma variavel chamada array e no meu console eu imprime (a) e a minha variavel,
+//que provavelmente vai estar undefined por não ter atribuido nenhum valor.
+//array = null
+//console.log('b. ', array)
+//aqui está apenas me informando que array não tem nenhum valor
+//no console esta imprimindo (b) e null
+
+//array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+//console.log('c. ', array.length)
+//aqui está me informando que (array que não está dentro de uma variavel) é um array(lista de 3 a 13)
+// aqui o console está imprimindo pramim (c) e que array é uma lista com 11 elementos
+
+//let i = 0
+//console.log('d. ', array[i])
+// aqui eu criei uma variavel (i)e atribui a ela o valor (0)
+//aqui o console vai me imprimir (3) porque o array está contando sua posição nas vogais
+
+//array[i+1] = 19
+//console.log('e. ', array)
+//aqui array[i+1]está atribuido o valor 19
+// o console vai me imprimir (e) e o valor 19 provavelmento array representa 15
+
+//const valor = array[i+6]
+//console.log('f. ', valor)
+// criamos a variavel valor = atribuido array[i+6]
+//aqui o console deve imprimir de volta (f)com o valor de 9
+
+//(2)(leia o codigo com atenção)
+
+//const frase = prompt("Digite uma frase")
+
+//console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
+
+//Qual será o valor impresso no console se a entrada do usuário for: "Subi num ônibus em Marrocos"?
+// "SUBA NUM ONABUS EM MARROCOS"
+
+
+
+
+/* - **Exercícios de escrita de código**
+    1. Faça um programa que pergunte ao usuário seu nome e seu e-mail. Em seguida, Imprima no console a seguinte mensagem:
+        
+        O e-mail `emailDoUsuario` foi cadastrado com sucesso. Seja bem-vinda(o), `nomeDoUsuario`!
+        
+        - 💡  Dica
+            
+            <aside>
+            ⭐ Você pode fazer isso de duas formas:
+            - Concatenando as strings (com o sinal de +)
+            - Utilizando template strings (e envelopando a sua string com o sinal de CRASE ao invés de aspas ⇒ ````)
+            
+            </aside>*/
+           /* let nomedousuario = prompt(`Nome de usuario por favor`)
+           let email = prompt(`email por favor`)
+
+           let template = `O e-mail ${email} foi cadastrado com sucesso. Seja bem-vinda(o), ${nomedousuario}`
+    
+           console.log(template) */
+    
+    
+    
+    
+    
+            /* 2. Faça um programa que contenha um array com 5 das suas comidas preferidas, armazenado em uma variável. Em seguida, siga os passos:
+        
+        a) Imprima no console o array completo
+        
+        b) Imprima no console a mensagem "Essas são as minhas comidas preferidas: ", seguida por cada uma das comidas, **uma embaixo da outra**.
+        
+        c) Aqui vai um desafio: pergunte ao usuário uma comida preferida. Troque a segunda comida da sua lista pela inserida pelo usuário. Imprima no consolea nova lista
+         */
+ /*   let comidasFavoriTas = ["macarrão", "pizza", "maçã", "feijoada", "nhoque"]
+   console.log(comidasFavoriTas)
+   let frase = "Essas são as minhas comidas preferidas:"
+   console.log(frase)
+   console.log(comidasFavoriTas[0])
+   console.log(comidasFavoriTas[1])
+   console.log(comidasFavoriTas[2])
+   console.log(comidasFavoriTas[3])
+   console.log(comidasFavoriTas[4])
+
+   let cUsuario = prompt("Por favor digite sua comida favorita")
+   comidasFavoriTas[1] = cUsuario
+   let noVaLista = "Essas são as minhas comidas preferidas atualisadas"
+   console.log(noVaLista)
+   console.log(comidasFavoriTas[0])
+   console.log(comidasFavoriTas[1])
+   console.log(comidasFavoriTas[2])
+   console.log(comidasFavoriTas[3])
+   console.log(comidasFavoriTas[4]) */ 
+   
+   
+   /* 3. Faça um programa, seguindo os passos:
+        
+        a) Crie um array vazio e guarde-o em uma variável, chamada `listaDeTarefas`
+        
+        b) Pergunte ao usuário 3 tarefas que ele precise realizar no dia e armazene-as, uma por uma, no array
+        
+        c) Imprima o array no console
+        
+        d) Peça ao usuário que digite o **índice** de uma tarefa que ele já realizou: 0, 1 ou 2 
+        
+        e) Remova da lista o item de índice que o usuário escolheu.
+        
+        f) Imprima o array no console */
+let listaDeTarefas = [] = prompt("Por favor digite 3 tarefas que voçê precise realizar durante o dia")
+console.log(listaDeTarefas)
+
+console.log(listaDeTarefas[0])
+console.log(listaDeTarefas[1])
+console.log(listaDeTarefas[2])
+
+let realizado = prompt("De 1 a 3 qual voçê ja realizou?")
+console.log(realizado-1)
+romovido = listaDeTarefas
+
+
+
+
+
+
