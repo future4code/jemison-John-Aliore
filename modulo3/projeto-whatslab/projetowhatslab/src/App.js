da57f9974f9012b41b92a019ed6e2c42c8d60368
john willian aliore <john.jwa123@gmail.com>
2022-07-08 22:43:45 -0300

    projetoWhatsLab

---
+++ modulo3/projeto-whatslab/projetowhatslab/src/App.js
@@ -0,0 +1,19 @@
+import react from 'react';
+import EnviarMsg from './components/ComponenteMsg';
+
+
+
+
+
+function WhatsLab() {
+return (
+<div classNamePai='Pai'>
+  <EnviarMsg/>
+
+
+</div>
+)
+
+}
+
+export default WhatsLab
