5a5d373a7de1531d25a59fa8805092a417b402ad
john willian aliore <john.jwa123@gmail.com>
2022-08-21 23:42:27 -0300

    subindo atrasado por questões tecnicas

--- modulo3/labex/labex/src/components/pages/HomePage.js
+++ modulo3/labex/labex/src/components/pages/HomePage.js
@@ -1,5 +1,44 @@
 import React from "react";
 import {useNavigate} from 'react-router-dom';
+import styled from "styled-components";
+
+const Container = styled.p`
+text-align:center;
+`
+
+const Title = styled.h1`
+
+position:absolute;
+text-align:center;
+align-items:center;
+left:30%;
+`;
+
+const ButtonTrip = styled.button`
+position:absolute;
+margin:10px;
+left:10px;
+`
+const ButtonLogin = styled.button`
+position:absolute;
+margin:10px;
+right:10px;
+`
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -17,13 +56,13 @@ function HomePage (){
  
 
     return(
-        <p>
-        <h1>Home</h1>
-     <button onClick={ goToAdmPag }>Login</button>
-     <button onClick={goToListTrip}>ListTrips</button>
+        <Container>
+        <Title>Viagens para o Espaço</Title>
+     <ButtonTrip onClick={ goToAdmPag }>Login</ButtonTrip>
+     <ButtonLogin onClick={goToListTrip}>ListTrips</ButtonLogin>
       
             
-        </p>
+        </Container>
     
     )
 }
--- modulo3/labex/labex/src/components/pages/ListTripsPage.js
+++ modulo3/labex/labex/src/components/pages/ListTripsPage.js
@@ -1,10 +1,23 @@
 import React from "react";
 import {useNavigate} from 'react-router-dom';
-
+import { BASE_URL} from '../Constants/Constants'
+import {useRequestData} from '../hook/useRequestData'
 
 
 
 function ListTripPage (){
+
+const [viagens,isLoadingLista,erro] = useRequestData(`${BASE_URL}trips`)
+
+const listaDviagens = viagens && viagens.trips.map((item) =>{
+  return( 
+  <div>
+  <p>{item.name}</p>
+   <p>{item.descripton}</p>
+   <p>{item.durationsInDays}</p>
+   <p>{item.date}</p>
+   </div>)
+})
 
     const navigate = useNavigate();
 const goToAppForm = () =>{
@@ -17,9 +30,19 @@ const goToHome = () =>{
         
         <p>
                   <h1>ListTripPage</h1>
+                  
      <button onClick={ goToAppForm }>ApplicationForm</button>
      <button onClick={goToHome}>Home</button>
-        </p>
+        
+
+
+{isLoadingLista&& <p> Carregando Usuario</p>}
+
+{!isLoadingLista&& viagens&&viagens.trips.length>0 && listaDviagens}
+{!isLoadingLista&& viagens&&viagens.trips.length === 0 && <p> Não ha Usuarios</p>}
+
+
+</p>
         
     )
 }
